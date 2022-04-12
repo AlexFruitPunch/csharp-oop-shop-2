e71fb2e71f5d3e3680ba77d6b9fc5f952d3efb06
alexpagani <alexpaganibr@gmail.com>
2022-04-12 17:18:43 +0200

    aggiunta controllo capienza

--- Csharp Shop 2/CiboInScatola.cs
+++ Csharp Shop 2/CiboInScatola.cs
@@ -7,7 +7,43 @@ using Csharp_Shop_2;
 
 namespace Csharp_Shop_2
 {
-    internal class CiboInScatola
+    internal class CiboInScatola : Prodotto
     {
+        private string luogodiProduzione;
+        private double peso;
+        private double capienzaMax;
+
+        public CiboInScatola(int codiceCiboInScatola, string nomeCiboInScatola, string descrizioneCiboInScatola, double prezzoCiboInScatola, string categoriaCiboInScatola, double peso, string luogodiProduzione) : base(codiceCiboInScatola, nomeCiboInScatola, descrizioneCiboInScatola, prezzoCiboInScatola, categoriaCiboInScatola)
+        {
+            this.luogodiProduzione = luogodiProduzione;
+            this.peso = peso;
+            capienzaMax = 300;
+            ControlloCapienza();
+        }
+
+        //------ Metodi ------
+
+        private void ControlloCapienza()
+        {
+            bool ImputCorretto = false;
+            while(ImputCorretto == false)
+            {
+                if (peso > capienzaMax)
+                {
+                    Console.WriteLine("non si può inserire più di 300 gr per lattina");
+                    Console.WriteLine("reinserisci un una grammatura giusta");
+                    peso = int.Parse(Console.ReadLine());
+                }else if (peso < 0)
+                {
+                    Console.WriteLine("il peso non può essere negativo");
+                    Console.WriteLine("reinserisci un una grammatura giusta");
+                    peso = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    ImputCorretto = true;
+                }
+            }
+        }
     }
 }
