3f728abb11f026cfc667d75f3200c9746c74b098
alexpagani <alexpaganibr@gmail.com>
2022-04-12 15:09:30 +0200

    Implementazione della sotto classe acqua

---
+++ Csharp Shop 2/Acqua.cs
@@ -0,0 +1,75 @@
+﻿using CsharpShop2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Shop_2
+{
+    internal class Acqua : Prodotto
+    {
+        private double litri; //litri inseriti/bevuti dalla bottiglia 
+        private double capienzaMassima;
+        private double capienzaAttuale;
+
+        public Acqua(int codiceAcqua, string nomeAcqua, string descrizioneAcqua, double prezzoAcqua, string categoriaAcqua, double litri, double capienzaMassima): base(codiceAcqua, nomeAcqua, descrizioneAcqua, prezzoAcqua, categoriaAcqua)
+        {
+            capienzaAttuale = 0;
+            this.litri = litri;
+            this.capienzaMassima = capienzaMassima;
+            ControlloLitri();
+        }
+
+        //------ Metodi ------ 
+
+        private void ControlloLitri()
+        {
+            if (litri < capienzaMassima )
+            {
+                capienzaAttuale = litri;
+            }
+            else if(litri == capienzaMassima)
+            {
+                capienzaAttuale = capienzaMassima;
+            }else{
+                Console.WriteLine("Hai sbordato la confezione");
+                Console.WriteLine("la bottiglia verrà riempita al massimo ma ci sarà un esubero");
+                capienzaAttuale = capienzaMassima;
+            }
+        }
+
+        //------ fine Metodi ------ 
+
+        //------ Getters ------ 
+
+        public double GetLitri()
+        {
+            return litri;
+        }
+
+        public double GetCapienzaMax()
+        {
+            return capienzaMassima;
+        }
+
+        //------ fine Getters ------ 
+
+        //------ Setters ------ 
+
+        public void SetLitri(double nuovilitri)
+        {
+            this.litri = nuovilitri;
+            ControlloLitri();
+        }
+
+        public void SetCapienzaMax(double capienzaMassima)
+        {
+            this.capienzaMassima = capienzaMassima;
+        }
+
+        //------ fine Setters ------ 
+    }
+
+
+}
--- Csharp Shop 2/Prodotto.cs
+++ Csharp Shop 2/Prodotto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CsharpShop
+namespace CsharpShop2
 {
     public class Prodotto
     {
