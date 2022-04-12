116debe5be8a980a13a81de0a9de4e4c0290a9f9
alexpagani <alexpaganibr@gmail.com>
2022-04-12 17:31:58 +0200

    inizio programma principale

--- Csharp Shop 2/Program.cs
+++ Csharp Shop 2/Program.cs
@@ -1,3 +1,15 @@
 ﻿using Csharp_Shop_2;
 
+Console.WriteLine("---------Gestionale Prodotti-----------");
+string nome;
+string descrizione;
+double prezzo;
+string categoria;
 
+Console.Write("nome prodotto:");
+nome = Console.ReadLine();
+Console.Write("inserisci descrizione");
+
+Console.Write();
+Console.Write();
+Console.Write();
